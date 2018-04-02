6d7b122241020f396b0e14708c7fa32492450bea
Liu <653064610@qq.com>
2018-04-02 10:36:45 +0800

    Revert "ceshi"
    This reverts commit b7e8280088f9c3df4dc8fb59823814e5d0d9f5d9.

--- venv/app.py
+++ venv/app.py
@@ -15,7 +15,7 @@ async def init(loop):
     srv = await loop.create_server(app.make_handler(), '127.0.0.1', 9300)
     logging.info('server started at http://127.0.0.1:9300...')
     return srv
-
+#5555
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
 loop.run_forever()
